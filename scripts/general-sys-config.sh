caf41afa860fd1ea458e44f8e36f0f2d31de7513
Nathan Glover <nathan@glovers.id.au>
2017-07-11 01:16:50 +0800

    Update general-sys-config.sh

--- scripts/general-sys-config.sh
+++ scripts/general-sys-config.sh
@@ -3,8 +3,17 @@
 # Install Python2.7 pip + dependencies
 sudo apt-get install python-pip
 
+# Install the python development packages
+sudo apt-get install python-dev
+
+# Install the bluetooth development packages
+sudo apt-get install libbluetooth-dev
+
 # Install pySerial for serial communication
 sudo pip install pyserial
+
+# Install the PyBluez bluetooth wrappers
+pip install pybluez
 
 # Make scripts directory
 mkdir /home/pi/scripts
