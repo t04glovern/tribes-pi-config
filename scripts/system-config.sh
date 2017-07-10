5ac43c94d9c60ec9f024bc57d52d12aa3f4bdc84
Nathan Glover <nathan@glovers.id.au>
2017-07-11 02:06:47 +0800

    Update system-config.sh

--- scripts/system-config.sh
+++ scripts/system-config.sh
@@ -3,5 +3,8 @@
 # Install Python2.7 pip + dependencies
 sudo apt-get install python-pip
 
+# Upgrade pip
+sudo pip install -U pip
+
 # Install the python development packages
 sudo apt-get install python-dev
