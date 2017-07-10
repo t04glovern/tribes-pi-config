79c1d7f0bd07df4f704da6e54521dc179814e72c
Nathan Glover <nathan@glovers.id.au>
2017-07-11 01:31:53 +0800

    Create bluetooth-config.py

---
+++ scripts/bluetooth-config.py
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install the bluetooth development packages
+sudo apt-get install libbluetooth-dev
+
+# Install pySerial for serial communication
+sudo pip install pyserial
+
+# Install the PyBluez bluetooth wrappers
+pip install pybluez
+
