dc5968071ca549ef57726fb6b9ef1f7e1881b79e
Nathan Glover <nathan@glovers.id.au>
2017-07-11 23:35:09 +0800

    Update 01-bluetooth-config.sh

--- scripts/01-bluetooth-config.sh
+++ scripts/01-bluetooth-config.sh
@@ -8,9 +8,15 @@ sudo apt-get install libbluetooth-dev
 # Install pybluez
 sudo pip install pybluez
 
+# Run the bluetooth daemon in compatibility mode (add '-C' after bluetoothd)
+sudo nano /lib/systemd/system/bluetooth.service
+
 # Reload the systemd daemon and restart bluetooth service
 sudo systemctl daemon-reload
 sudo systemctl restart bluetooth
+
+# Load serial port profile
+sudo sdptool add SP
 
 # Configure the bluetooth serial interface
 sudo hciconfig hci0 up
