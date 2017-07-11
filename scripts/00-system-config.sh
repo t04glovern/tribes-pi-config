9569ac16f5b7603b63621d767cb59031134501fa
Nathan Glover <nathan@glovers.id.au>
2017-07-11 20:37:13 +0800

    Update 00-system-config.sh

--- scripts/00-system-config.sh
+++ scripts/00-system-config.sh
@@ -2,10 +2,10 @@
 
 # Install Python2.7 pip + dependencies
 wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --user
+sudo python get-pip.py --user
 
 # Upgrade pip
-pip install -U pip
+sudo pip install -U pip
 
 # Install the python development packages
 sudo apt-get install python-dev
