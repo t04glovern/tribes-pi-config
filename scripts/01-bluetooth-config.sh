72fe365648e5a3c56c65139cffdd8433327c6737
Nathan Glover <nathan@glovers.id.au>
2017-07-11 22:14:17 +0800

    Update 01-bluetooth-config.sh

--- scripts/01-bluetooth-config.sh
+++ scripts/01-bluetooth-config.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
 
-# Install the bluetooth development packages
-sudo apt-get install libbluetooth-dev
-
-# Install the bluetooth low engery rependencies
-sudo apt-get install pkg-config libboost-python-dev libboost-thread-dev libglib2.0-dev
-
-# Install the PyBluez bluetooth low energy wrappers
-sudo pip install pybluez[ble]
-
-# Append --compat to the ExecStart line (someone write the sed for this)
-#sudo nano /lib/systemd/system/bluetooth.service
-
-# Reload the systemd daemon and restart bluetooth service
-sudo systemctl daemon-reload
-sudo systemctl restart bluetooth
-
 # Configure the bluetooth serial interface
-sudo hciconfig hci0 piscan
 sudo hciconfig hci0 name 'NODE-001'
 sudo hciconfig hci0 up
+sudo hciconfig hci0 sspmode 1
+sudo hciconfig hci0 piscan
+sudo bluetooth-agent 1234
