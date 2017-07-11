ad7f07def1b4647c162308ff47991827c40f2364
Nathan Glover <nathan@glovers.id.au>
2017-07-11 22:16:37 +0800

    Update 01-bluetooth-config.sh

--- scripts/01-bluetooth-config.sh
+++ scripts/01-bluetooth-config.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+sudo echo PRETTY_HOSTNAME=raspberrypi > /etc/machine-info
+
+# Install the bluetooth development packages
+sudo apt-get install libbluetooth-dev
+
+# Reload the systemd daemon and restart bluetooth service
+sudo systemctl daemon-reload
+sudo systemctl restart bluetooth
+
 # Configure the bluetooth serial interface
-sudo hciconfig hci0 name 'NODE-001'
 sudo hciconfig hci0 up
 sudo hciconfig hci0 sspmode 1
 sudo hciconfig hci0 piscan
