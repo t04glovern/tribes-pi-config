97f2a70a8bd3ef19a022d8f1f4e75a6ac0970f12
Nathan Glover <nathan@glovers.id.au>
2017-07-13 02:46:48 +0800

    reformated i2c file

--- setup-scripts/02-i2c-uart.sh
+++ setup-scripts/02-i2c-uart.sh
@@ -14,7 +14,7 @@ if [ "$(id -u)" != 0 ]; then
   echo 'Sorry, you need to run this script with sudo'
   exit 1
 fi
- 
+
 # enable I2C on Raspberry Pi
 echo '>>> Enable I2C'
 if grep -q 'i2c-bcm2708' /etc/modules; then
@@ -43,7 +43,7 @@ if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
 else
   echo 'File raspi-blacklist.conf does not exist, skip this step.'
 fi
- 
+
 # install i2c-tools
 echo '>>> Install i2c-tools'
 if hash i2cget 2>/dev/null; then
@@ -53,5 +53,5 @@ else
 fi
 
 #configure UART
-<TODO>
-https://www.raspberrypi.org/documentation/configuration/uart.md
+#<TODO>
+#https://www.raspberrypi.org/documentation/configuration/uart.md
