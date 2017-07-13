7a03c7c4e4607e04fd2a563da0e21395de3f249f
Nathan Glover <nathan@glovers.id.au>
2017-07-13 09:47:25 +0000

    fixed formatting

--- config.sh
+++ config.sh
@@ -1,23 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 # Change hostname if needed
-echo ">>>Hostname must be changed for node network"
-if [ "$(cat /etc/hostname)" == "raspberrypi" ]; then
-    echo "Enter Node name, followed by [ENTER]:"
+current_hostname="$(cat /etc/hostname)"
+rasp='raspberrypi'
+
+echo '>>>Hostname must be changed for node network'
+if [ "$current_hostname" == "$rasp" ]; then
+    echo 'Enter Node name, followed by [ENTER]:'
     read node_name
     while true; do
         read -p "To confirm, node_name is ${node_name}? [y/n]" yn
         case $yn in
             [Yy]* ) break;;
-            [Nn]* ) echo "Please re-run the script with the correct input"; exit;;
-            * ) echo "Please answer y/n.";;
+            [Nn]* ) echo 'Please re-run the script with the correct input'; exit;;
+            * ) echo 'Please answer y/n.';;
         esac
     done
 
     echo ">>>Changing hostname to ${node_name}"
-    original_hostname="$(cat /etc/hostname)"
-    sudo sed -i "s%$original_hostname%$node_name%g" /etc/hostname
-    sudo sed -i "s%$original_hostname%$node_name%g" /etc/hosts
+    sudo sed -i "s%$current_hostname%$node_name%g" /etc/hostname
+    sudo sed -i 's%$current_hostname%$node_name%g' /etc/hosts
 
     echo ">>>Committing name to ${node_name} system"
     sudo /etc/init.d/hostname.sh
@@ -25,4 +27,3 @@ if [ "$(cat /etc/hostname)" == "raspberrypi" ]; then
     echo ">>>Rebooting system to apply hostname"
     sudo reboot
 fi
-
